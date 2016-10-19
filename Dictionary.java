aa59185cb5341604b173d75e529f2e140044f117
N03048839 <giannitj2@hawkmail.newpaltz.edu>
2016-10-19 09:43:11 -0400

    Dictionary class rebuild
    A rebuild of the Dictionary class. List of changes:
    - Words are now stored in arrays rather than an additional data structure (better space efficiency)
    - Word selection now retrieves ONE word that matches criteria, rather than the entire set of words that match
    Need to implement:
    - For the word selection algorithm to work efficiently, words need to be sorted by part of speech and syllable count

--- Dictionary.java
+++ Dictionary.java
@@ -2,380 +2,273 @@ package haiku;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.Set;
 
-
-public class Dictionary 
+/**
+ * <p>
+ * This class manages the loading, parsing, and sorting of words provided in a 
+ * dictionary ".dic" file.
+ * </p>
+ * 
+ * <p><b>Input file format</b></p>
+ * A dictionary file is used to store words. A dictionary file begins with a header,
+ * which specifies the number of words listed in the file, as well as any string tokens
+ * used as delimiters within the file.
+ * and then lists its words, each followed by its respective part of speech.
+ * 
+ * 
+ * <br>
+ * Headers use the following format:
+ * 
+ *   <br><br>
+ *   <code> &lt;H lines=<i>val</i>; delim="<i>d</i>";></code>
+ *   <br><br>
+ * 
+ * where <code><i>val</i></code> is the number of words stored in the file, 
+ * and <code><i>d</i></code> is the delimiter token seperating words from
+ * their parts of speech.
+ *   
+ * 
+ * <br><br>
+ * @author Jobin
+ *
+ */
+public class Dictionary
 {	
-	//-- stores information about loaded words
-	private Map<String, PartOfSpeech> dictionary;
-	
-	
 	/**
-	 * Creates an empty Dictionary.
+	 * Constructs a new Dictionary that manages pre-loaded words.
+	 * @param filename a dictionary file to be loaded
+	 * @throws FileNotFoundException if specified file cannot be found
+	 * @throws IllegalStateException if specified file is improperly formatted
 	 */
-	public Dictionary() 
+	public Dictionary(String filename) throws FileNotFoundException, IllegalStateException 
 	{
-		dictionary = new HashMap<String, PartOfSpeech>();
-	}
-	
-	
-	/**
-	 * This constructor only loads the specified dictionary text file into memory.
-	 * @param filename the dictionary text file to initially load
-	 * @throws IOException specified dictionary txt file not found
-	 */
-	public Dictionary(String filename) throws IOException 
-	{
-		dictionary = new HashMap<String, PartOfSpeech>();
+		randGen = new java.util.Random();
+		final String errmsg = "Error loading dictionary file \"" + filename + "\": ";
 		
-		load(filename);
-	}
-	
-	
-	/**
-	 * Read the given dictionary text file, and add its contents to this class' internal dictionary.
-	 * 
-	 * @param filename the filename of a dictionary text file
-	 * 
-	 * @throws FileNotFoundException if the given file cannot be located
-	 * 
-	 * @return true if the dictionary file was loaded successfully
-	 */
-	public boolean load(String filename) throws IOException
-	{
-		if (filename == null)
-			return false;
-		if (!filename.toUpperCase().endsWith(".TXT"))
-			return false;
-
-		Scanner inFile = new Scanner(new File(filename));		
-
-		while(inFile.hasNextLine()) 	
-			loadItem(inFile.nextLine());
+		System.out.println(" ==== Initializing Dictionary ====");
+		
+		System.out.print("Loading dictionary file...");
+		// ---- Create File scanner ---- //
+		Scanner inFile;
+		try {
+			inFile = new Scanner( new File(filename) );
+		} catch (FileNotFoundException e) {
+			throw new FileNotFoundException(errmsg + "file not found!");
+		}
+		System.out.println("loaded");
+		
+		// ---- Strip file header ---- //
+		System.out.print("Stripping file header...");
+		String header = inFile.findWithinHorizon("<H.*>", 200);
+		if (header == null) {
+			inFile.close();
+			throw new IllegalStateException(errmsg + "missing file header!");
+		}
+		
+		// ---- Parse header ---- //
+		final String l_token = "lines=";
+		final String d_token = "delim=\"";
+		
+		int l_stPos = header.indexOf(l_token) + l_token.length();
+		int l_endPos = header.indexOf(';', l_stPos);
+		int d_stPos = header.indexOf(d_token) + d_token.length();
+		int d_endPos = header.indexOf('\"', d_stPos);
+		
+		if (l_stPos < 1 || l_endPos < 1 || d_stPos < 1 || d_endPos < 1) {
+			inFile.close();
+			throw new IllegalStateException(errmsg + "invalid file header!");
+		}
+		
+		final String delim = header.substring(d_stPos, d_endPos);
+		//TODO: debug statement
+		System.out.println("delim: \"" + delim + "\"");
+		size = Integer.parseInt(header.substring(l_stPos, l_endPos));
+		System.out.println("done");
+		
+		// ---- Instantiate data arrays ---- //
+		allWords = new String[size];
+		wordSyllables = new int[size];
+		wordPOS = new PartOfSpeech[size];
+		
+		System.out.print("Parsing dictionary...");
+		// ---- Process remainder of file ---- //
+		
+		inFile.nextLine();   // prime inFile scanner (header stripping leaves blank line)
+		for (int i = 0; inFile.hasNextLine() && i < size; i++) 
+		{	
+			String line = inFile.nextLine();
+			//TODO: debug statement
+			if (!line.contains(delim))
+				System.out.println("\nError parsing line " + i + ": doesn't contain delim: \"" 
+									+ delim + "\"\n" + line);
+			else {
+				String word = line.substring(0, line.indexOf(delim));
+				
+				// -- store the word to next slot in arrays -- //
+				allWords[i] = word;
+				wordPOS[i] = parsePOS(line);
+				wordSyllables[i] = SyllableCounter.syllables(word);
+				if (wordSyllables[i] > maxSyllableCount)
+					maxSyllableCount = wordSyllables[i];
+			}
+		}
 		
 		inFile.close();
-		return true;
+		System.out.println("done");
+		
+		System.out.print("Sorting arrays... [NOT YET IMPLEMENTED -- SKIPPING]\n");
+		//TODO: bucketsort the dictionary arrays
+		
+		setArrayPointers();
+		System.out.println(" ====== DICTIONARY INITIALIZED ======");
 	}
 	
-	
-	private boolean loadItem(String entry)
-	{
-		// this line enforces entry format, (word | PARTOFSPEECH) without parenthesis 
-		if (!entry.matches(".*|.*")) 
-			return false;
-		
-		//prune everything in line before delimiter (inclusive)
-		String posString = entry.substring(entry.indexOf('|') + 1);
-		
-		//prune everything in line after delimiter
-		String word = entry.substring(0, entry.indexOf('|'));
-		
-		if(posString.contains(" ADJECTIVE")) 
-		{
-			dictionary.put(word, PartOfSpeech.ADJECTIVE);
-			return true;
-		}
-		
-		if(posString.contains(" ADVERB")) 
-		{
-			dictionary.put(word, PartOfSpeech.ADVERB);
-			return true;
-		}
-		
-		if(posString.contains(" PREPOSITION")) 
-		{
-			dictionary.put(word, PartOfSpeech.PREPOSITION);
-			return true;
-		}
-		
-		if(posString.contains(" ARTICLE")) 
-		{
-			dictionary.put(word, PartOfSpeech.ARTICLE);
-			return true;
-		}
-		
-		if(posString.contains(" NOUN")) 
-		{
-			dictionary.put(word, PartOfSpeech.NOUN);
-			return true;
-		}
-		
-		if(posString.contains(" VERB"))
-		{
-			dictionary.put(word, PartOfSpeech.VERB);
-			return true;
-		}
-		
-	
-		return false;
-	}
-
 	
 	/**
-	 * Saves the dictionary loaded in memory to a specified text file.
+	 * Get the number of stored words with {@link PartOfSpeech} <code>p</code>
+	 * and <code>m</code> syllables.
 	 * 
-	 * @return true if the dictionary was saved successfully to the text file.
+	 * @param m number of syllables
+	 * @param p PartOfSpeech of the desired words
 	 */
-	public boolean save(String filename) 
-	{
-		if (filename == null)
-			return false;
-		if (!filename.toUpperCase().endsWith(".TXT"))
-			return false;
+	public int subsetSize(PartOfSpeech p, int m) {
+		final String errMsg = "Error counting " + m + "-syllable " + p + "s: ";
+		if (p == null)
+			throw new IllegalArgumentException(errMsg + "POS is null!");
+		if (m < 1)
+			throw new IllegalArgumentException(errMsg + "too few syllables!");
+		if (m > maxSyllableCount)
+			throw new IllegalArgumentException(errMsg 
+					+ "too many syllables! (max = " + maxSyllableCount + ")");
 		
-		try {
-			PrintWriter outFile = new PrintWriter(filename);
-			for( Entry<String, PartOfSpeech> element : dictionary.entrySet())
-				outFile.println(element.getKey() + " | " + element.getValue());
-			
-			outFile.close();
-			return true;
-			
-		} catch (IOException exception) {
-			exception.printStackTrace();
-			return false;
-		}
-	}
-	
-	
-	/**********************************************************\
-	 *	The following methods deal with reading and modifying 
-	 * 	word elements in the internal dictionary.
-	 *
-	\**********************************************************/
-	
-	/**
-	 * Returns a set containing all dictionary words with the given part of speech.
-	 */
-	public Set<String> wordSet(PartOfSpeech pos) 
-	{
-		Set<String> set = new HashSet<String>();
-		
-		for( Entry<String, PartOfSpeech> element : dictionary.entrySet())
-			if(element.getValue() == pos)
-				set.add(element.getKey());
-		
-		return set;
+		int q = indexOfPOS(p);
+		return sz[q][m];
 	}
 	
 	/**
-	 * Returns a set containing all dictionary words that have both the specified part of speech,
-	 *  and the specified number of syllables
-	 */
-	public Set<String> wordSet(PartOfSpeech pos, int syl) 
-	{
-		Set<String> set = new HashSet<String>();
-		
-		for( Entry<String, PartOfSpeech> item : dictionary.entrySet())
-			if(item.getValue() == pos)
-				if(sylCount(item.getKey()) == syl)
-					set.add(item.getKey());
-		
-		return set;
-	}
-	
-	/**
-	 * Returns a set containing all the dictionary words that have the specified part of speech, 
-	 * as well as a syllable count between sMin and sMax (inclusive).
+	 * Randomly select a word with the given part of speech and syllable count.
 	 * 
-	 * @param pos the part of speech
-	 * @param sMin the smallest number of syllables usable
-	 * @param sMax the largest number of syllables usable
+	 * This method retrieves the entire set of internally stored words meeting the criteria,
+	 * and selects one at random.
+	 * 
+	 * @param p Desired part of speech
+	 * @param m syllables in word
+	 * @return the word
+	 * @throws IllegalArgumentException if p is null, or if no word exists that matches
 	 */
-	public Set<String> wordSet(PartOfSpeech pos, int sMin, int sMax) 
+	public String getRandomWord(PartOfSpeech p, int m) {
+		final String errMsg = "Error getting random " + m + "-syllable " + p + ": ";
+		if (p == null || p == PartOfSpeech.BLANK)
+			throw new IllegalArgumentException(errMsg + "invalid Part of speech \'" + p + "\'");
+		if (m < 1) { 
+			//throw new IllegalArgumentException(errMsg + "too few syllables!");
+			return null;
+		}
+		if (m > maxSyllableCount) {
+			//throw new IllegalArgumentException(errMsg 
+			//+ "too many syllables! (max = " + maxSyllableCount + ")");
+			return getRandomWord(p, m-1);
+		}
+		
+		int px = indexOfPOS(p);
+		if (sz[px][m] < 1) {
+			//throw new IllegalArgumentException(errMsg + "no " + m + "-syllable words!");
+			return null;
+		}
+		
+		int r = randGen.nextInt(sz[px][m]);
+		return allWords[si[px][m] + r];
+	}
+	
+	
+	private void setArrayPointers() 
 	{
-		Set<String> set = new HashSet<String>();
+		System.out.println(" ---- Setting array pointers ---- ");
+		System.out.println("POS count: "+ POS_COUNT + "\nmax syllables: " + maxSyllableCount);	
 		
-		System.out.println("INITIAL WORDSET SIZE: " + set.size()
-				+ "\n Populating with " + pos + " with length between " + sMin + " and " + sMax);
+		//		index 'i' corresponds to words with 'i' syllables
+		si = new int[POS_COUNT][maxSyllableCount + 1];
+		sz = new int[POS_COUNT][maxSyllableCount + 1];
 		
-		for(Entry<String, PartOfSpeech> item : dictionary.entrySet())
-			if(item.getValue() == pos) 
+		
+		//---		populate start index table 		---//
+		for (int wi = 1, p = 0, m = 1; 
+				wi < allWords.length && p < POS.length-1; wi++) {
+			
+			// iterate through words, looking for where POS changes
+			if (wordPOS[wi] != wordPOS[wi-1]) 
 			{
-				int syl = sylCount(item.getKey());
+				while (++m <= maxSyllableCount)
+					si[p][m] = -1;
 				
-				if(sMin <= syl && syl <= sMax)
-					set.add(item.getKey());
+				si[p][0] = wi-1;
+				si[++p][1] = wi;
+				m = 1;
 			}
-		System.out.println("RETURNING WORD SET WITH SIZE: " + set.size());
-		return set;
-	}
-	
-	/**
-	 *  Returns the part of speech of the given word.
-	 */
-	public PartOfSpeech getPOS(String word) {
-		return dictionary.get(word);
-	}
-	
-	
-	
-	
-	/**********************************************************\
-	 *	The following methods deal with syllable analysis.
-	 *
-	\**********************************************************/
-	
-	/**
-	 * Counts the number of syllables in a word.
-	 * Returns 0 if input word is null.
-	 */
-	public static int sylCount(String word) 
-	{
-		if(word == null) 
-			return 0;
-		
-		word = word.trim().toUpperCase();	
-		int vowels = vowelCount(word);
-		
-		if(vowels > 1)
 			
-			/*
-			 *  The number of vowels is decreased if the word ends in -ed,
-			 *  or -ly
-			 */
-			if(word.matches(".*[A-Z && [^AEIOUY]]ED?" + "LY?"))
-				vowels--;
-		
-		return vowels - diphCount(word);
-	}
-	
-	/**
-	 * Counts the number of diphthongs (one-syllable vowel pairs) in a word.
-	 */
-	private static int diphCount(String word) 
-	{
-		if (word == null)
-			return 0;
-		
-		word = word.toUpperCase();
-		
-		int count = 0;
-		if(word.matches(".*A[EIUY].*")) 	count++;
-		if(word.matches(".*E[AEIUY].*")) 	count++;
-		if(word.matches(".*I[AEOU].*")) 	count++;
-		if(word.matches(".*O[AIOUY].*")) 	count++;
-		if(word.matches(".*U[AEIUY].*")) 	count++;
-		if(word.matches(".*[A-Z&&[^AEIOU]]Y[AEIOU].*")) count++;
-		
-		return count;
-	}
-	
-	/**
-	 * Counts the number of vowels in a word.
-	 */
-	public static int vowelCount(String word) 
-	{
-		if (word == null)
-			return 0;
-		
-		int count = 0;
-		for(int i = 0; i < word.length(); i++)
-			if(isVowel(word.charAt(i)))
-				count++;
-		
-		return count;
-	}
-	
-	
-	/**
-	 * Determines whether a given character is a vowel.
-	 */
-	public static boolean isVowel(char c) 
-	{
-		switch(c) 
-		{
-		case 'A': 
-		case 'a':	
-		case 'E': 
-		case 'e':
-		case 'I': 
-		case 'i':
-		case 'O': 
-		case 'o':
-		case 'U': 
-		case 'u':
-		case 'Y': 
-		case 'y':
-				return true;
-		default: 	
-				return false;
-		}
-	}
-
-	
-	/**********************************************************\
-	 *	The following methods provide information about the underlying data.
-	 *
-	\**********************************************************/
-	
-	/**
-	 * Returns true if this dictionary contains no elements (words).
-	 */
-	public boolean isEmpty() {
-		return dictionary.isEmpty();
-	}
-	
-	
-	/**
-	 * Returns the number of entries in this dictionary.
-	 */
-	public int size() {
-		return dictionary.size();
-	}
-	
-	
-	/**
-	 * Returns true if this dictionary contains the specified word.
-	 */
-	public boolean contains(String word) {
-		return dictionary.containsValue(word);
-	}
-	
-	
-	/**
-	 * Returns an array containing all of the words in this dictionary.
-	 */
-	public String[] toArray() 
-	{	
-		String[] array = new String[dictionary.size()];
-		
-		int i = 0;
-		for(String item: dictionary.keySet()) 
-		{
-			array[i] = item;
-			i++;
+			// iterate through words, looking for where syl count changes
+			else if (wordSyllables[wi] != wordSyllables[wi-1]) 
+			{
+				while (++m < wordSyllables[wi]) 
+					si[p][m] = -1;	
+				if (m == wordSyllables[wi])
+					si[p][m] = wi;
+			}
 		}
 		
-		return array;
+		//---		populate size table 		---//
+		for (int p = 0; p < POS.length; p++)
+			for (int m = 1; m <= maxSyllableCount; m++) {
+				if (m == maxSyllableCount)
+					sz[p][m] = (si[p][m] < 0)?  0 : si[p][0] - si[p][m];
+				else
+					sz[p][m] = (si[p][m] < 0 && si[p][m+1] < 0)?  0 : si[p][m+1] - si[p][m];
+			}
+		System.out.println(" ---- Array pointers set ---- ");
 	}
 	
-
-	/**
-	 * Add the specified word to this dictionary.
-	 * 
-	 * @return true if the new word was added successfully
-	 */
-	public boolean add(String word, PartOfSpeech pos) 
-	{
-		if (word == null || pos == null)
-			return false;
+	
+	private int indexOfPOS(PartOfSpeech p) {
+		for (int i = 0; i < POS.length-1; i++)
+			if (p.equals(POS[i]))
+				return i;
 		
-		if (word.length() == 0)
-			return false;
-		
-		if (pos == PartOfSpeech.BLANK)
-			return false;
-
-		dictionary.put(word, pos);
-		return true;
+		return -1;
 	}
+	
+	
+	static PartOfSpeech parsePOS(String s) {
+		final String errMsg = "Error parsing Part of speech from \"" + s + "\": ";
+		if (s.equals(""))
+			throw new IllegalArgumentException(errMsg + "null string!");
+		
+		for (int i = 0; i < POS.length; i++) {
+			if (s.contains(POS[i].toString()))
+				return POS[i];
+		}
+		
+		return null;
+	}
+	
+	
+	// -------------- INTERNAL COMPONENTS ---------- //
+	private String[] allWords;
+	private int[] wordSyllables;
+	private PartOfSpeech[] wordPOS;
+	private int size;
+	
+	private static final PartOfSpeech[] POS = PartOfSpeech.values();
+	private static final int POS_COUNT = POS.length;
+	private static int maxSyllableCount;
+	
+	/* The following two arrays use the following format:
+	 * 		si[p][m]
+	 * 		p:= int corresponding to part of speech
+	 * 		m:= number of syllables
+	 */
+	private int[][] si;			//	stores start indices for words matching criteria
+	private int[][] sz;			//	stores number of words matching criteria
+
+	private java.util.Random randGen;
 }
